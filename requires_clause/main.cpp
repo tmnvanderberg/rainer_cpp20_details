a43cced703dbfef9f2625a736397248c59bf7045
Timon van der Berg <tmnvanderberg@gmail.com>
2024-11-24 17:08:42 +0100

    concepts 1

---
+++ requires_clause/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <unsigned int i>
+requires (i <= 20)
+int sum (int j) {
+	return i + j;
+}
+
+int main() {
+	std::cout << "\n";
+	std::cout << "sum<20>(2000): " << sum<20>(2000) << '\n';
+	std::cout << "\n";
+}
+
+
